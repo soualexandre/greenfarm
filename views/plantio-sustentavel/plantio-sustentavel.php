f0aaf7995695eb1ca436f4f3cd667ad7dfa093c9
Alexandre Souza dos Santos <55295879+xande-dev@users.noreply.github.com>
2019-10-11 15:04:44 -0300

    commit

---
+++ views/plantio-sustentavel/plantio-sustentavel.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("../inicial/modelo/cabecalho_app.php");
+
+?>
+
+<html>
+<head>
+
+
+</head>
+<body>
+    <h4 class="text-center" >Temos algumas dicas de sustentabilidade para que o seu plantio continue a crescer</h4>
+
+
+
+
+</body>
+
+</html>
