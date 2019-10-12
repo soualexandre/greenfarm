ca63ea12d999680ce98f5b53a323d863bd735359
Alexandre <alexandreifto2@gmail.com>
2019-10-12 02:00:00 -0300

    alterdo

--- views/plantio-sustentavel/plantio-sustentavel.php
+++ views/plantio-sustentavel/plantio-sustentavel.php
@@ -4,32 +4,66 @@ require_once("../inicial/modelo/cabecalho_app.php");
 
 ?>
 <html>
+
 <head>
-<link type="text/css" href="sustentavel.css" >
+    <link type="text/css" href="sustentavel.css">
 </head>
+
 <body>
-    <h4 class="text-center"id="titulo-sust" >Temos algumas dicas de sustentabilidade para que o seu plantio continue a crescer</h4>
+
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../../public/img/cultsust1.jpg " class="d-block w-100" alt="Foto cultura sustentável">
+            </div>
+            <div class="carousel-item">
+                <img src="../../public/img/cultsust.jpg" class="d-block w-100" alt="Foto cultura sustentável">
+            </div>
+            <div class="carousel-item">
+                <img src="../../public/img/cultsust2.jpg" class="d-block w-100" alt="Foto cultura sustentável">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Proximo</span>
+        </a>
+    </div>
     <hr>
-    <ul>
-       
+    <h4 class="text-center" id="titulo-sust">Temos algumas dicas de sustentabilidade para que o seu plantio continue a crescer</h4>
+
+    <hr>
+
     <ul>
         <li>
-            Utilizar solo sem contaminantes;
+            <h6> Utilizar solo sem contaminantes;</h6>
+            <label for="">
+                O solo é um recurso finito, limitado e não renovável, diante a sua crescente degradação das últimas décadas e a sua lenta formação e regeneração, é conveniente que se tenha uma maior responsabilidade no uso deste recurso para que se possa garantir a sua utilização por muito tempo.
+            </label>
         </li>
         <li>
-            Se possível, esterilizar o solo antes de plantar (solarização);
+            <h6>
+                Se possível, esterilizar o solo antes de plantar (solarização);</h6>
+            <label for="">A solarização é uma técnica muito efetiva, que tem efeito similar à esterilização do solo por vapor. Suas vantagens são o baixo custo, simplicidade e o fato de não ser perigosa. O método não usa substâncias químicas, é de fácil aplicação e não deixa resíduos tóxicos nas plantas. A solarização é um processo hidrotérmico que esteriliza o solo por meio de calor gerado naturalmente, matando as plantas daninhas.<br>
+
+                Esse método envolve a colocação de um plástico transparente sobre um solo preparado para o cultivo e úmido para que permita que a energia solar aqueça o solo e mate as sementes de plantas daninhas. Para ser mais efetiva, a solarização deve ser realizada durante os períodos de máxima exposição à radiação solar do ano (verão). Há alguns passos envolvidos na solarização que devem ser seguidos para o sucesso da técnica:</label>
         </li>
         <li>
             Comprar mudas sadias;
         </li>
         <li>
-            Não exagerar na adubação;
+           <h6> Não exagerar na adubação</h6>
+           <label for=""> Cada planta tem um ritmo de crescimento próprio e precisa ser adubada com determinada frequência. Por exemplo; algumas plantas de rápido crescimento, como os gerânios, podem ser adubadas até uma vez por semana no período de crescimento. Já outras, como os cactos e as suculentas, não precisam mais de uma ou duas aplicações por ano.
+<br>
+O ideal é saber a dosagem e a frequência que cada espécie precisa, e prestar atenção aos sinais de adubação excessiva: <ul> <li> crescimento exagerado das hastes</li> <li> limo esverdeado na superfície do vaso</li> <li> folhas com pontas amarronzadas</li></ul></label>
         </li>
         <li>
             Irrigar corretamente;
         </li>
     </ul>
-</ul>
 </body>
 
 </html>
